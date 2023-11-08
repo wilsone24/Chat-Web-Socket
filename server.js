e9b7ad55259b24f5b85fca464fb4898d2b13450f
wilsone24 <wilsone@uninorte.edu.co>
2023-11-07 23:36:49 -0500

    Test

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('¡Hola, mundo!\n');
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}/`);
+});
