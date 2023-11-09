59b0b90c715f970324b72661cb580efb1e4a8022
wilsone24 <wilsone@uninorte.edu.co>
2023-11-08 20:42:23 -0500

    Test

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
+response = "Wilson";
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('¡Hola, mundo!\n');
