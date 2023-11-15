eee5563b665e5e6b4699e2b1b1bd8b8d67f09d46
wilsone24 <wilsone@uninorte.edu.co>
2023-11-14 22:00:42 -0500

    Database and save the messages

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import {pool} from './db.js'
 
 
 
@@ -11,21 +12,39 @@ const server = createServer(app) /* Create http server */
 const io = new Server(server) /* Create socket server */
 
 
-io.on('connection',(socket) => { /* when the socket receive a connection, then do this */
+io.on('connection',async (socket) => { /* when the socket receive a connection, then do this */
     console.log('a user has connected')
 
     socket.on('disconnect', () => { /* when the socket receive a disconnection, then do this */
         console.log('a user has disconnected')})
 
-    socket.on('chat message',(msg) => {
-        io.emit('chat message', msg) /* Send the message to all the sockets connected */
+    socket.on('chat message',async (msg) => {
+        await pool.query('INSERT INTO messages (content) VALUES (?)', msg)
+        const [result] = await pool.query('SELECT * FROM messages')
+        console.log(result)
+        const [row] = await pool.query('SELECT LAST_INSERT_ID();')
+        io.emit('chat message', msg, row[0]['LAST_INSERT_ID()'])
     })
+    if (!socket.recovered){ 
+        try {
+            const [results] = await pool.query('SELECT * FROM messages WHERE id > ?', [socket.handshake.auth.serverOffset ?? 0]);
+            results.forEach((item) => {
+                socket.emit('chat message', item.CONTENT, item.id.toString())
+            })
+        } catch (e) {
+           console.error(e) 
+        }
+    }
+    
+    
 })
 
 app.use(logger('dev')) /* Get info about the status of the server */
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+/*     const [result] = await pool.query('SELECT * FROM messages')
+    console.log(result) */
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
